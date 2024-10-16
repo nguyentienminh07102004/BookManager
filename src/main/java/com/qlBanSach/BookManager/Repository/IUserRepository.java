bcf7081b6f4956bde42c49a10f7df666575c8b68
Nguyen Tien Minh <nguyentienminhntm07102004@gmail.com>
2024-10-16 22:51:06 +0700

    update demo

--- src/main/java/com/qlBanSach/BookManager/Repository/IUserRepository.java
+++ src/main/java/com/qlBanSach/BookManager/Repository/IUserRepository.java
@@ -1,9 +1,6 @@
 package com.qlBanSach.BookManager.Repository;
 
 import com.qlBanSach.BookManager.Model.Entity.UserEntity;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
